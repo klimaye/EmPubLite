66273cae6bab77f9adae36a32a9a4498a2b03725
Kshitij Limaye <klimaye@gmail.com>
2013-04-07 15:24:41 -0500

    removed target folder

--- empublite-it/target/generated-sources/r/com/commonsware/empublite/test/Manifest.java
+++
@@ -1,5 +0,0 @@
-package com.commonsware.empublite.test;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
--- empublite/target/generated-sources/r/com/commonsware/empublite/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.commonsware.empublite;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
